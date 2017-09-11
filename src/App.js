3eebe3c3833daf48a2fbd1bf0b490b15d2046ac5
Dave Guymon <davidwguymon@gmail.com>
2017-09-10 21:21:19 -0600

    Installed and set up Sass for create-react-app. Created simple navigation using Sass.

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Social</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default App;
